04b2d12a0976ddfed66f78b0e77a225eebdee5f0
zynomon <92979020+zynomon@users.noreply.github.com>
2026-03-10 23:15:02 +0600

    y

---
+++ build/Desktop_Qt_6_10_2_MinGW_64_bit-MinSizeRel/CMakeFiles/clean_additional.cmake
@@ -0,0 +1,10 @@
+# Additional clean files
+cmake_minimum_required(VERSION 3.16)
+
+if("${CONFIG}" STREQUAL "" OR "${CONFIG}" STREQUAL "MinSizeRel")
+  file(REMOVE_RECURSE
+  "CMakeFiles\\vex_autogen.dir\\AutogenUsed.txt"
+  "CMakeFiles\\vex_autogen.dir\\ParseCache.txt"
+  "vex_autogen"
+  )
+endif()
---
+++ build/Desktop_Qt_6_10_2_MinGW_64_bit-Release/.qt/QtDeployTargets.cmake
@@ -0,0 +1,3 @@
+set(__QT_DEPLOY_TARGET_vex_FILE C:/Users/fast/Documents/GitHub/vex/build/Desktop_Qt_6_10_2_MinGW_64_bit-Release/vex.exe)
+set(__QT_DEPLOY_TARGET_vex_TYPE EXECUTABLE)
+set(__QT_DEPLOY_TARGET_vex_RUNTIME_DLLS C:/Qt/6.10.2/mingw_64/bin/Qt6Widgets.dll;C:/Qt/6.10.2/mingw_64/bin/Qt6Gui.dll;C:/Qt/6.10.2/mingw_64/bin/Qt6Core.dll)
